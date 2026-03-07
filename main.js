8844eb17b8cf6dcd9b328dd9c41cfff8847dfa75
MCbasketball000 <3811737414@qq.com>
2026-03-07 10:36:19 +0800

    1

--- main.js
+++ main.js
@@ -1,36 +1,28 @@
+function initall(){
+    document.body.style.backgroundColor = "#101010";
+}
 //一些乱七八糟的读取
-var resourceRegister;
+var resourceRegister;//注册表变量
+var haveData;//游戏数据变量
 async function initregister()
 {
     const response = await fetch("./register/resource.json");
     resourceRegister = await response.json();
     const response2 = await fetch("./lang/zh-cn.json");
     languageRegister = await response2.json();
-    var haveData;
-    //存档部分（本来想单独写成一个文件的但是不会导入就算了
-    function load(){
-        haveData = localStorage.getItem("haveData");
-    }
-    function save(){
-        localStorage.setItem("haveData", haveData);
-    }
-    function init(){
-        haveData = true;
-        save();
-    }
-    load();
-    if(haveData != true){
-        init();
-    }
-    //-------------------------------------------------//
     return 0;
 }
 
 
 
-function CreateElementBy(type,content,pos,pos1,pos2,opcode,lengthz,widthz){
+function CreateElementBy(type,pos,pos1,pos2,opcode,lengthz,widthz,content){
     var newDiv = document.createElement(type);
-    newDiv.textContent = content;
+    if(content != undefined){
+        newDiv.textContent = content;
+    }
+    else{
+        newDiv.textContent = languageRegister.opcode;
+    }
     newDiv.style.position = "absolute";
     newDiv.id = opcode;
     //pos：1是左上，2是右上，3是左下，4是右下
@@ -80,10 +72,27 @@ function getLockedElement(){
 function refreshResourse(){
     var keys = Object.keys(resourceRegister);
     keys.forEach(function(element, index) {
-        CreateElementBy("div",keys[index],1,index*10,0,JSON.stringify(resourceRegister[element]))
+        CreateElementBy("div",1,index*10,0,JSON.stringify(resourceRegister[element]))
     });
 }    
 function main(fuck){
+    //存档部分（本来想单独写成一个文件的但是不会导入就算了
+    function load(){
+        haveData = localStorage.getItem("haveData");
+    }
+    function save(){
+        localStorage.setItem("haveData", haveData);
+    }
+    function init(){
+        haveData = true;
+        save();
+    }
+    load();
+    if(haveData != true){
+        init();
+    }
+    //-------------------------------------------------//
+
     console.log(fuck);
     console.log(resourceRegister.garbage);
     
